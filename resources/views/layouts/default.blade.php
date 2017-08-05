07bf0b4905761b8e225e8e91c9ac9e99fd346331
lzw95 <zcboy95@gmail.com>
2017-08-05 04:18:38 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>@yield('title', 'sample') - 学习Laravel</title>
+	</head>
+	<body>
+		@yield('content')
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
